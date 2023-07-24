dfc99f371539f7ca091c1a20b1d4c4fcd3410d21
giorgialari <g.giorgialari@gmail.com>
2023-07-24 08:40:34 +0200

    add rounting and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SingleProductComponent } from './components/single-product/single-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'single-product', component: SingleProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingleProductComponent } from './components/single-product/single-product.component';
+import { NavbarComponent } from './shared/ui/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleProductComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
