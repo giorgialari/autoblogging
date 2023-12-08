e376307cec01b1eb82c3ffe1b2ffc2e7724ecf91
giorgialari <g.giorgialari@gmail.com>
2023-12-08 16:20:28 +0100

    fix env file

--- src/enviroment/enviroment.prod.ts
+++ src/enviroment/enviroment.prod.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: false,
-  // openAI_apiKey: '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',
-  // wp_wpUrl: 'https://reviewmodeon.com/wp-json/wp/v2',
-  // wp_status: 'draft',
-  // wp_btoa: 'r3vi3wm0d30n:fj[^1le84j24',
   apiUrl: 'https://karlai-api.onrender.com',
-  // apiDbUrl: 'https://karlai-db.onrender.com'
-  apiDbUrl: 'http://localhost:3000'
+  apiDbUrl: 'https://karlai-db.onrender.com'
 };
